61bbad5648ff46b23225149b6a66fc5a5a3b098a
Fred <fredjgermain@gmail.com>
2021-01-31 20:35:43 -0500

    ActiveHook
    Seperate active hook functionalities from dao

--- src/reusable/customhooks/useactive.hook.tsx
+++ src/reusable/customhooks/useactive.hook.tsx
@@ -1,8 +1,34 @@
-import {}
+import React, {useContext, useState} from 'react'; 
+import {useDao, EActionType, IUseDao} from '../dao2/usedao2.hook'; 
 
 
-/* 
-activeCollection 
-activeEntry 
-activeMode 
-*/ 
+// ICrudContext ------------------------------------------- 
+interface ICrudContext extends IUseDao { 
+  collection: ICollection; 
+  setCollection: React.Dispatch<ICollection>; 
+  entry: IEntry; 
+  setEntry: React.Dispatch<IEntry>; 
+  mode: string; 
+  setMode: React.Dispatch<string>; 
+} 
+export const CrudContext = React.createContext({} as ICrudContext); 
+
+
+
+// CrudContexter ========================================== 
+export function CrudContexter() { 
+  const {} = useContext(CrudContext); 
+  const {} = useContext(); 
+
+} 
+
+
+
+// UseCrud ================================================ 
+export function useCrud() { 
+  const {GetICollections} = useContext(DaoContext); 
+
+  const [collection, setCollection] = useState({} as ICollection); 
+  const [entry, setEntry] = useState({} as IEntry); 
+  const [mode, setMode] = useState(''); 
+} 
