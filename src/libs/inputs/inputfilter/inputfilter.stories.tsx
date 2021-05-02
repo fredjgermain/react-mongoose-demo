60822d9a95ffdd9a0efd1ef41cd5431afe57da09
Fred <fredjgermain@gmail.com>
2021-05-01 21:53:14 -0400

    fix filtersorter stories

--- src/libs/inputs/inputfilter/inputfilter.stories.tsx
+++ src/libs/inputs/inputfilter/inputfilter.stories.tsx
@@ -1,38 +1,32 @@
 import { Story } from '@storybook/react'; 
 import { InputFilter, useFilter } from './_inputfilter'; 
-import { IndexArray } from '../../_arrayutils'; 
+import { InputSorter, useSorter } from '../inputsort/_inputsort'; 
 
 
 function TemplateResearch({values, filters}:{values:any[], filters:{handle:string, type:string}[]}) { 
-  const indexedValues = IndexArray(values); 
-  const {filteredValues, SetFilters} = useFilter(indexedValues); 
+  const {matchValues, SetFilters} = useFilter(values); 
+  const {sortedValues, SetSorters} = useSorter(matchValues); 
 
   return <div> 
-    <div> Original <br/> 
-      {indexedValues.map( (value) => { 
-        return <div key={value.i}> 
-          {JSON.stringify(value.t)} 
-        </div> 
-      })} </div> 
-
-    <br/>Filters <br/> 
-    {filters.map( ({handle, type}, i) => { 
-      const keys = handle ? ['t', handle]: ['t']; 
-      return <span key={i}> 
-        {handle}: 
-        <InputFilter {...{ type, keys, SetFilters }} /> 
-      </span> 
-    })} 
-    <br/>Filtered <br/> 
-    {filteredValues.map( (value,i) => { 
-      return <div key={i}>{JSON.stringify(value)}</div> 
-    })} 
+    <div> 
+      {sortedValues.map( (v,i) => { 
+        return <div key={i}>{JSON.stringify(v)}</div> 
+      })} 
+    </div> 
+    <div> 
+      {filters.map( (f,i) => { 
+        return <span key={i}> 
+          <InputFilter {...{ type:f.type, keys:[f.handle], SetFilters }} /> 
+          <InputSorter {...{ type:f.type, keys:[f.handle], SetSorters }} /> 
+        </span>
+      })} 
+    </div> 
   </div> 
 } 
 
 
 export default { 
-  title: 'input/Filter', 
+  title: 'input/FilterSorter', 
   component: TemplateResearch 
 } 
 
@@ -40,20 +34,9 @@ const Template:Story<any> = args => <TemplateResearch {...args} />
 
 
 
-// Editor One Value ===========================================
-/*const predicates:Predicate<number>[] = [ 
-  (t:number) => t % 2 === 0, 
-  (t:number) => t < 10, 
-]
-export const TestFilters = Template.bind({}) 
-TestFilters.args = { 
-  values:[1,2,3,5,4,6,9,7,8,10,45,12,14], 
-  predicates: predicates, 
-}*/
-
 export const LambdaFilter = Template.bind({}) 
 LambdaFilter.args = { 
-  values:[1,2,3,4,6,8,9], 
+  values:[3,4,1,2,6,8,9], 
   filters:[{handle:'', type:'number'}] 
 } 
   
